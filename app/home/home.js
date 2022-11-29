60a62b2847b7cdbe078fa8e8006c7386e6731093
TheKingLouLi <louayhouimli@engineer.com>
2022-11-29 19:52:44 +0100

    Update home.js

--- app/home/home.js
+++ app/home/home.js
@@ -32,7 +32,7 @@ homeModule.config([
 
                     var templateUrl = "https?://github.com/.+/.+";
                     var downloadUrlInfix = "#/home?url=";
-                    var downloadUrlPrefix = "https://minhaskamal.github.io/DownGit/"+downloadUrlInfix;
+                    var downloadUrlPrefix = "https://loulination.github.io/LouLiNation/"+downloadUrlInfix;
 
                     if ($routeParams.url) {
                         $scope.url = $routeParams.url;
